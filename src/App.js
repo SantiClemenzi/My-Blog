658f44afc708c77e354b49553763b9a7e0a3d063
santiago clemenzi <clemenzisantiago@gmail.com>
2022-01-25 18:29:04 -0300

    estilos_creado

--- src/App.js
+++ src/App.js
@@ -1,6 +1,9 @@
 import React from 'react';
+// react router
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
-// importamos los componentes
+// styled component
+import styled from 'styled-components';
+// componentes
 import Header from './components/Header';
 import Home from './components/Home';
 import Blog from './components/Blog';
@@ -9,18 +12,29 @@ import About from './components/About';
 const App = () => {
 	return (
 		<BrowserRouter>
-			<div>
-        <Header/>
-				<main>
+			<ContenedorPrincipal>
+				<Header />
+				<Main>
 					<Routes>
 						<Route path="/" element={<Home />} />
 						<Route path="/blog" element={<Blog />} />
 						<Route path="/about" element={<About />} />
 					</Routes>
-				</main>
-			</div>
+				</Main>
+			</ContenedorPrincipal>
 		</BrowserRouter>
 	);
 };
 
+const ContenedorPrincipal = styled.div`
+	padding: 40px;
+	width: 90%;
+	max-width: 700px;
+`;
+const Main = styled.main`
+	background-color: #fff;
+	padding: 40px;
+	border-radius: 10px;
+	box-shadow: 0px 0px 5px rgba(129, 129, 129, 0.1);
+`;
 export default App;
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,17 +1,40 @@
 import React from 'react';
 import { NavLink } from 'react-router-dom';
+import styled from 'styled-components';
 
 const Header = () => {
 	return (
-		<header>
-			<h1>My Blog</h1>
-			<nav>
+		<ContenedorHeader>
+			<Titulo>My Blog</Titulo>
+			<Menu>
 				<NavLink to="/">Home</NavLink>
 				<NavLink to="/blog">Blog</NavLink>
 				<NavLink to="/about">About me</NavLink>
-			</nav>
-		</header>
+			</Menu>
+		</ContenedorHeader>
 	);
 };
+const ContenedorHeader = styled.header `
+	text-align: center;
+	margin-bottom: 40px;
+`;
+const Titulo = styled.h1 `
+	margin-bottom: 10px;
+	font-size: 26px;
+	text-transform: uppercase;
+`;
+const Menu = styled.nav `
+	a{
+		text-decoration: none;
+		color: #165168;
+		margin: 0px 10px;
+	}
+	a:hover{
+		color: #000;
+	}
+	a.active{
+		border-bottom: 2px solid #165168;
+	}
+`;
 
 export default Header;
